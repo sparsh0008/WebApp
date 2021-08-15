8300f2b8ed086957ad38aad78d3699e7d8126eac
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2021-08-15 14:48:53 +0530

    Translator in Progress

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput     = document.querySelector("#txt-input");
+var outputDiv    = document.querySelector("#output");
+
+
+function clickEventHandler() {
+    outputDiv.innerText = "jhasdvlashdvljkafdvh : " + txtInput.value;
+};
+
+btnTranslate.addEventListener("click", clickEventHandler);
