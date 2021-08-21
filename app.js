ebfb793898d8f4084f626d16e427ec51017f32f1
sparsh0008 <54952775+sparsh0008@users.noreply.github.com>
2021-08-21 23:04:52 +0530

    Completed

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput     = document.querySelector("#txt-input");
 var outputDiv    = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text
+};
 
 function clickEventHandler() {
-    outputDiv.innerText = "Output : \n" + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
 };
 
 btnTranslate.addEventListener("click", clickEventHandler);
